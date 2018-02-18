cc1805eb28de8e521e61413ed027ca679bcfabe8
Sena Candra MPC <senacandrampc@gmail.com>
2018-02-18 10:43:23 +0700

    Added modify, delete, and help

--- routes/linebot.js
+++ routes/linebot.js
@@ -16,7 +16,7 @@ router.post('/webhook', function(req, res, next) {
 				if (message.type == 'text') {
 					const { text } = message;
 					const { userId } = source;
-					const groupId = process.env.GROUP_ID || source.groupId;
+					const groupId = process.env.DEV_GROUP_ID || source.groupId;
 					if (text.charAt(0) == '/') {
 						const cmd = text.split(' ');
 						switch (cmd[0].toLowerCase()) {
@@ -76,33 +76,40 @@ router.post('/webhook', function(req, res, next) {
 												});
 											}
 											else {
-                                                var user = null;
-                                                try{
-                                                    user = await client.getProfile(userId);
-                                                } catch(e) {
-                                                    console.log(e);
-                                                }
-                                                if(!user){
-                                                    client.replyMessage(replyToken, {
-                                                        type: 'text',
-                                                        text: 'Please add me as a friend before proceeding.'
-                                                    });
-                                                }
-                                                else {
-                                                    const { displayName } = user;
-                                                    event.participants.push({
-                                                        userId,
-                                                        displayName
-                                                    });
-                                                    await event.save();
-                                                    const message = await this.getStatus(groupId, eventId);
-                                                    if (message) {
-                                                        client.replyMessage(replyToken, {
-                                                            type: 'text',
-                                                            text: message
-                                                        });
-                                                    }
-                                                }
+												var user = null;
+												try {
+													user = await client.getProfile(userId);
+												} catch (e) {
+													console.log(e);
+												}
+												if (!user) {
+													client.replyMessage(replyToken, [
+														{
+															type: 'text',
+															text: "I can't recognize you well to perform this action."
+														},
+														{
+															type: 'text',
+															text:
+																'Please add me first so I can know you better (please!)'
+														}
+													]);
+												}
+												else {
+													const { displayName } = user;
+													event.participants.push({
+														userId,
+														displayName
+													});
+													await event.save();
+													const message = await this.getStatus(groupId, eventId);
+													if (message) {
+														client.replyMessage(replyToken, {
+															type: 'text',
+															text: message
+														});
+													}
+												}
 											}
 										}
 									} catch (err) {
@@ -138,8 +145,8 @@ router.post('/webhook', function(req, res, next) {
 									]);
 								}
 								else {
-                                    var command = cmd.slice(0, 2);
-                                    command.push(cmd.slice(2).join(" "));
+									var command = cmd.slice(0, 2);
+									command.push(cmd.slice(2).join(' '));
 									const eventId = command[1].toLowerCase();
 									const eventDescription = command[2];
 									var event = await Event.findOne({
@@ -197,6 +204,140 @@ router.post('/webhook', function(req, res, next) {
 								}
 								break;
 							}
+							case '/modify': {
+								if (cmd.length <= 2) {
+									client.replyMessage(replyToken, [
+										{
+											type: 'text',
+											text: 'Invalid command. Please type in an event id'
+										},
+										{
+											type: 'text',
+											text: '/modify [eventId] [new event description]'
+										}
+									]);
+								}
+								else {
+									const eventDescription = cmd.slice(2).join(' ');
+									const event = await Event.findOne({
+										$and: [
+											{
+												groupId: groupId
+											},
+											{
+												eventId: cmd[1]
+											}
+										]
+									});
+									if (!event) {
+										client.replyMessage(replyToken, {
+											type: 'text',
+											text: "Can't find the specified event ID."
+										});
+									}
+									else {
+										if (event.userId == userId) {
+											event.eventDescription = eventDescription;
+											await event.save();
+											const message = await this.getStatus(groupId, cmd[1]);
+											client.replyMessage(replyToken, [
+												{
+													type: 'text',
+													text: 'Event description saved'
+												},
+												{
+													type: 'text',
+													text: message
+												}
+											]);
+										}
+										else {
+											client.replyMessage(replyToken, {
+												type: 'text',
+												text: 'Only the creator of the event can delete the event.'
+											});
+										}
+									}
+								}
+								break;
+							}
+							case '/delete': {
+								if (cmd.length <= 1) {
+									client.replyMessage(replyToken, [
+										{
+											type: 'text',
+											text: 'Invalid command. Please type in an event id'
+										},
+										{
+											type: 'text',
+											text: '/delete [eventId]'
+										}
+									]);
+								}
+								else {
+									const event = await Event.findOne({
+										$and: [
+											{
+												groupId: groupId
+											},
+											{
+												eventId: cmd[1]
+											}
+										]
+									});
+									if (!event) {
+										client.replyMessage(replyToken, {
+											type: 'text',
+											text: "Can't find the specified event ID."
+										});
+									}
+									else {
+										if (event.userId == userId) {
+											await Event.findOneAndRemove({
+												$and: [
+													{
+														groupId: groupId
+													},
+													{
+														eventId: cmd[1]
+													}
+												]
+											});
+											client.replyMessage(replyToken, {
+												type: 'text',
+												text: 'Event has been successfully deleted'
+											});
+										}
+										else {
+											client.replyMessage(replyToken, {
+												type: 'text',
+												text: 'Only the creator of the event can delete the event.'
+											});
+										}
+									}
+								}
+								break;
+							}
+							case '/help': {
+								client.replyMessage(replyToken, {
+									type: 'text',
+									text:
+										'How to use Event Bot\n\n' +
+										'/create [eventId] [event description]\n' +
+										'Create a new event. Event ID must be unique and may not contain whitespace\n\n' +
+										'/join [eventId]\n' +
+										'Join an event.\n\n' +
+										'/modify [eventId] [new event description]\n' +
+										'Modify the event description of an event\n\n' +
+										'/delete [eventId]\n' +
+										'Delete the event\n\n' +
+										'/status\n' +
+										'Check all active events in the group\n\n' +
+										'/status [eventId]\n' +
+										'Check event information and participants'
+								});
+								break;
+							}
 						}
 					}
 				}
@@ -229,8 +370,8 @@ getStatus = async (groupId, eventId) => {
 		var count = 1;
 		event.participants.forEach((e) => {
 			message += `${count++}. ${e.displayName}\n`;
-        });
-        message += `\nType '/join ${event.eventId}' to join.`;
+		});
+		message += `\nType '/join ${event.eventId}' to join.`;
 		return message;
 	}
 };
@@ -244,8 +385,8 @@ getEvents = async (groupId) => {
 	message += 'Events in this group:\n';
 	events.forEach((e) => {
 		message += `${count++}. ${e.eventId}\n`;
-    });
-    message += "\nType '/status [eventId]' to check the status of the event."
+	});
+	message += "\nType '/status [eventId]' to check the status of the event.";
 	return message;
 };
 
